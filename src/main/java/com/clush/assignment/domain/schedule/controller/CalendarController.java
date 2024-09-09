d76abd4d6696fd588e53e68368b7418d239209e2
hajeu <s22072@gsm.hs.kr>
2024-09-09 14:57:25 +0900

    add: 캘린더 업데이트 기능 구현

--- src/main/java/com/clush/assignment/domain/schedule/controller/CalendarController.java
+++ src/main/java/com/clush/assignment/domain/schedule/controller/CalendarController.java
@@ -6,6 +6,7 @@ import com.clush.assignment.domain.schedule.dto.response.BasicCalendarResDto;
 import com.clush.assignment.domain.schedule.service.calendar.CreateCalendarService;
 import com.clush.assignment.domain.schedule.service.calendar.QueryAllCalendarsService;
 import com.clush.assignment.domain.schedule.service.calendar.QueryCalendarsService;
+import com.clush.assignment.domain.schedule.service.calendar.UpdateCalendarByIdService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ public class CalendarController {
     private final CreateCalendarService createCalendarsService;
     private final QueryCalendarsService queryCalendarsService;
     private final QueryAllCalendarsService queryAllCalendarsService;
+    private final UpdateCalendarByIdService updateCalendarByIdService;
 
     @PostMapping
     public ResponseEntity<Void> create(
@@ -40,5 +42,14 @@ public class CalendarController {
     @GetMapping("/all")
     public ResponseEntity<List<BasicCalendarResDto>> findAll() {
         return ResponseEntity.ok(queryAllCalendarsService.execute());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(
+            @PathVariable("id") Long id,
+            @RequestBody BasicCalendarReqDto basicCalendarReqDto
+    ) {
+        updateCalendarByIdService.execute(id, basicCalendarReqDto);
+        return ResponseEntity.noContent().build();
     }
 }
