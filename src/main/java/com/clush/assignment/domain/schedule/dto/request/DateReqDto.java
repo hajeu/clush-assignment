4744789c65e925426b5666abe829bec5fb3b5769
하제우 <103640011+hajeu@users.noreply.github.com>
2024-09-10 20:39:11 +0900

    Update DateReqDto.java

--- src/main/java/com/clush/assignment/domain/schedule/dto/request/DateReqDto.java
+++ src/main/java/com/clush/assignment/domain/schedule/dto/request/DateReqDto.java
@@ -1,8 +1,11 @@
 package com.clush.assignment.domain.schedule.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record DateReqDto(
+        @NotNull
         LocalDate reqDate
 ) {
 }
